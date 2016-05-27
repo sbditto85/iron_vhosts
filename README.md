5af361514a025d6b9bf3b4ece3a0c760daf05927
Casey Allred <sbditto85@gmail.com>
2016-05-27 16:54:02 -0600

    touch up examples

--- README.md
+++ README.md
@@ -6,18 +6,31 @@ Iron Vhosts
 ## Example
 
 ```rust
+extern crate iron;
+extern crate iron_vhosts;
+
+use iron::prelude::*;
+use iron::status;
+use iron_vhosts::Vhosts;
+
 fn main () {
     //Default handler passed to new
     let mut vhosts = Vhosts::new(|_: &mut Request| Ok(Response::with((status::Ok, "vhost"))));
-
+    
     //Add any host specific handlers
     vhosts.add_host("localhost", localhost_handler);
-
+    vhosts.add_host("media.localhost", media_handler);
+    
     fn localhost_handler(_: &mut Request) -> IronResult<Response> {
         Ok(Response::with((status::Ok, "localhost")))
     }
-}
 
+    fn media_handler(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "media")))
+    }
+
+    Iron::new(vhosts).http("localhost:3000").unwrap();
+}
 ```
 
 ## Installation
--- src/lib.rs
+++ src/lib.rs
@@ -25,22 +25,29 @@ impl Vhosts {
     ///
     /// ```
     /// extern crate iron;
-    /// extern crate url;
     /// extern crate iron_vhosts;
-    /// 
-    /// use iron::{Handler, IronResult, Request, Response, status};
+    ///
+    /// use iron::prelude::*;
+    /// use iron::status;
     /// use iron_vhosts::Vhosts;
-    /// 
+    ///
     /// fn main () {
     ///     //Default handler passed to new
     ///     let mut vhosts = Vhosts::new(|_: &mut Request| Ok(Response::with((status::Ok, "vhost"))));
-    ///     
+    ///
     ///     //Add any host specific handlers
     ///     vhosts.add_host("localhost", localhost_handler);
-    ///
+    ///     vhosts.add_host("media.localhost", media_handler);
+    ///   
     ///     fn localhost_handler(_: &mut Request) -> IronResult<Response> {
     ///         Ok(Response::with((status::Ok, "localhost")))
     ///     }
+    ///
+    ///     fn media_handler(_: &mut Request) -> IronResult<Response> {
+    ///         Ok(Response::with((status::Ok, "media")))
+    ///     }
+    ///
+    ///     Iron::new(vhosts).http("localhost:3000").unwrap();
     /// }
     /// ```
     pub fn add_host<H: Handler>(&mut self, host: &'static str, h: H) -> Option<Box<Handler>> {
